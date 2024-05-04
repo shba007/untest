782479b5888f4aebfeb6bddec0ce6cbd093b6533
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-05-04 23:40:55 +0530

    feat: temp fix

--- pages/result.vue
+++ pages/result.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import confetti from 'canvas-confetti';
+import type { Stats } from 'fs';
 
 const userStore = useUserStore()
 const testStore = useTestStore()
@@ -25,9 +26,14 @@ onMounted(() => {
     </section>
     <h2 class="text-xl">{{ userStore.name?.split(' ')[0] }}'s Report</h2>
     <section v-if="data" class="mb-auto flex justify-between items-start w-full">
-      <PointCard topic="correct" :point="data?.correctCount" color="blue" />
-      <PointCard topic="wrong" :point="data?.incorrectCount" color="red" />
-      <PointCard topic="duration" :point="data?.duration" color="green" />
+      <PointCard topic="correct" :point="data.correctCount" color="blue" />
+      <PointCard topic="wrong" :point="data.incorrectCount" color="red" />
+      <PointCard topic="duration" :point="data.duration" color="green" />
+    </section>
+    <section v-else class="mb-auto flex justify-between items-start w-full">
+      <PointCard topic="correct" :point="testStore.stats.correctCount" color="blue" />
+      <PointCard topic="wrong" :point="testStore.stats.incorrectCount" color="red" />
+      <PointCard topic="duration" :point="testStore.stats.duration" color="green" />
     </section>
     <AppButton title="Go to Leaderboard" href="/leaderboard" class="mt-auto" />
   </main>
